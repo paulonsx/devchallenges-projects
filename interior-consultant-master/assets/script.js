5dff8c212c95a57cfb653e2815a484f770b5e2db
Paulo <paulovma20@gmail.com>
2022-07-19 00:05:32 -0300

    codigo refatorado

--- interior-consultant-master/assets/script.js
+++ interior-consultant-master/assets/script.js
@@ -2,8 +2,10 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
+
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    
     const logo = document.getElementById('logo');
     logo.classList.toggle('active');
 }
