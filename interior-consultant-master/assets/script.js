5279260c5cef05f8fd3ce1a9a4e0fb1b371049fb
Paulo <paulovma20@gmail.com>
2022-07-18 23:06:44 -0300

    layout pronto e refatorando classes e unidades

--- interior-consultant-master/assets/script.js
+++ interior-consultant-master/assets/script.js
@@ -4,6 +4,8 @@ function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    const logo = document.getElementById('logo');
+    logo.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
